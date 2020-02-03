03dbb72e841d778e71705830e23dc501332ea4b3
MarjanGit <mansar16@huntersoe.org>
2020-02-02 21:37:03 -0500

    Updates

---
+++ precache-manifest.fac331f6a4ccdc54b31b6c541e707702.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "09f424e4197c0e3c3b6369429cff8146",
+    "url": "/medp341-class-website/index.html"
+  },
+  {
+    "revision": "7270dec7516e43404d0a",
+    "url": "/medp341-class-website/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "cbbfee68639ae640ffef",
+    "url": "/medp341-class-website/static/js/2.53336d99.chunk.js"
+  },
+  {
+    "revision": "928d7b5eb39d16fe9a880722c974b51e",
+    "url": "/medp341-class-website/static/js/2.53336d99.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7270dec7516e43404d0a",
+    "url": "/medp341-class-website/static/js/main.17883ce8.chunk.js"
+  },
+  {
+    "revision": "3fd7f396c77dd5a65f41",
+    "url": "/medp341-class-website/static/js/runtime-main.bee6056a.js"
+  }
+]);
